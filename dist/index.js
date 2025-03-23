8312016fc8c72654b4dea574c206e08ceb50f187
unknown <scobru1988@gmail.com>
2025-03-23 19:32:33 +0100

    updates

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,4 @@
 import { ShogunButton, ShogunButtonProvider, useShogun } from './components/ShogunButton';
 import { shogunConnector } from './connectors/shogun';
-import './types/shogun-sdk'; // Assicuriamo che i tipi estesi vengano riconosciuti
 export { ShogunButton, ShogunButtonProvider, useShogun, shogunConnector };
 export * from './types';
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import { ShogunButton, ShogunButtonProvider, useShogun } from './components/ShogunButton';
 import { shogunConnector } from './connectors/shogun';
-import './types/shogun-sdk'; // Assicuriamo che i tipi estesi vengano riconosciuti
 
 export { ShogunButton, ShogunButtonProvider, useShogun, shogunConnector };
 export * from './types';
